592d7c18e31ffa4741182dad768072e20243c072
felipe cook <felipepcook@gmail.com>
2022-05-24 19:54:24 +0800

    learning about difference between . and .()

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ function Book(title, author, numOfPages, haveRead){
   this.numOfPages = numOfPages,
   this.haveRead = haveRead
   this.info = function() {
-    return `This book has ${numOfPages} pages, the title is ${title}, the author is ${author}, and it has been read: ${haveRead}.`
+    return `This book has ${numOfPages} pages, the title is ${title}, the author is ${author}, and it has been read: ${haveRead}.`;
   }
 }
 
 
 const theHobbit = new Book("The Hobbit", "JRR Tolk", 57, true);
 
-console.log(theHobbit.info);
+console.log(theHobbit.info());
