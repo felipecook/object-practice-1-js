68a65b66e60908b452682bb507f419e77897e96b
felipe cook <felipepcook@gmail.com>
2022-05-25 19:33:27 +0800

    completed addBookToLibary function

--- app.js
+++ app.js
@@ -11,12 +11,29 @@ function Book(title, author, numOfPages, haveRead){
 }
 
 
-const theHobbit = new Book("The Hobbit", "JRR Tolk", 57, true);
+//const theHobbit = new Book("The Hobbit", "JRR Tolk", 57, true);
 
 console.log(theHobbit.info());
 
 function addBookToLibrary() {
-  let book = prompt("What book title would you like to add?")
-  library.push(book);
+  const newBook = new Book("Undefined", "Undefined", 0, false);
+  let bookTitle = prompt("What book title would you like to add?");
+  let bookAuthor = prompt("What is the books author?");
+  let bookLength = prompt("What is the length of the book?");
+  let haveIReadIt = prompt("Have you read this book? True or False?");
+  if(haveIReadIt == "True"){
+    haveIReadIt = true;
+  } else {
+    haveIReadIt = false;
+  }
+
+
+  newBook.title = bookTitle;
+  newBook.author = bookAuthor;
+  newBook.numOfPages = bookLength;
+  newBook.haveRead = haveIReadIt;
+  
+  console.log(newBook.info());
+  library.push(newBook);
 }
 
