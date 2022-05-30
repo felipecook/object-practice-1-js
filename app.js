cc5bd938762c9f8b328e9edde1c665548e906df2
felipe cook <felipepcook@gmail.com>
2022-05-30 19:31:24 +0800

    added display library

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const body = document.querySelector("body__container");
+
+
+
 let library = [];
 
 function Book(title, author, numOfPages, haveRead){
@@ -36,5 +40,25 @@ function addBookToLibrary() {
 
 
 function displayLibrary () {
-  
+
+  for (let index = 0; index < library.length; index++) {
+    const element = library[index];
+
+    let individualBook = document.createElement('div');
+    individualBook.classList.add(book);
+    body.appendChild(individualBook);
+    let bookTitle = document.createElement('h4');
+    bookTitle.innerHTML = element.bookTitle;
+    let bookAuthor = document.createElement('h5');
+    bookAuthor.innerHTML = element.bookAuthor;
+    let bookLength = document.createElement('h6');
+    bookLength.innerHTML = "Pages : " + element.bookLength;
+    let haveIReadIt = document.createElement('h6');
+    haveIReadIt.innerHTML = "Have I read this : " + element.haveIReadIt;
+    individualBook.appendChild(bookTitle);
+    individualBook.appendChild(bookAuthor);
+    individualBook.appendChild(bookLength);
+    individualBook.appendChild(haveIReadIt);
+    
+  }
 }
